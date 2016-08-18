e1224c07748c34ceb2692718b0f3337bf54b04f8
matthew <matthewjf@gmail.com>
2016-08-18 01:23:56 -0700

    setup

---
+++ app/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className='card-panel'>
+        Hello from Home Component
+      </div>
+    );
+  }
+}
+
+export default Home;
