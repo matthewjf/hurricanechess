68e003248808653cd9c825ed49a34c9fdb67bbed
matthew <matthewjf@gmail.com>
2016-09-25 16:30:46 -0700

    use correct config var for mongodb

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   port:  process.env.PORT || 3000,
   secret: process.env.MONGO_SECRET || 'ChessXSuperCool',
-  database: process.env.MONGO_DB || 'localhost/cx',
+  database: process.env.MONGODB_URI || 'localhost/cx',
   sessions: process.env.MONGO_SESSIONS || 'localhost/sessions',
   redis: process.env.REDIS_URL
 };
