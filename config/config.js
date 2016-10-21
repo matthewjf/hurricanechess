4b480ef54f7f3243d4ca7698e7b071614e48575f
matthew <matthewjf@gmail.com>
2016-10-20 22:13:52 -0700

    remove

--- config/config.js
+++ config/config.js
@@ -4,6 +4,5 @@ module.exports = {
   secret: process.env.MONGO_SECRET || 'ChessXSuperCool',
   database: process.env.MONGODB_URI || 'localhost/cx',
   redis: process.env.REDIS_URL,
-  // sendgrid_api_key: process.env.SENDGRID_API_KEY
-  sendgrid_api_key: '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'
+  sendgrid_api_key: process.env.SENDGRID_API_KEY
 };
